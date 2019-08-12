affb94a9514d0e9f147628a01b637125e800ab68
rachelfranklin1 <rachel.franklin@marvelapp.com>
2019-08-12 21:49:02 +0100

    beginning of Modal

--- worldkind/src/pages/index.js
+++ worldkind/src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import styled from "styled-components"
 
 import Layout from "../components/Layout"
+import Modal from "../components/Modal"
 import Nav from "../components/Navigation"
 import World from "../media/images/world.jpg"
 
@@ -13,6 +14,7 @@ const Image = styled.img`
 export default () => (
   <Layout>
     <Nav text="World Kind" />
+    <Modal />
     <Image src={World} alt="" />
   </Layout>
 )
