f88face504d7ba9f64de696a6db623af9c5139f7
rachelfranklin1 <rachel.franklin@marvelapp.com>
2019-08-17 13:18:27 +0100

    figure out styling in gatsby

--- worldkind/src/pages/index.js
+++ worldkind/src/pages/index.js
@@ -1,20 +1,36 @@
 import React from "react"
 import styled from "styled-components"
+import ReactModal from "react-modal"
 
 import Layout from "../components/Layout"
-import Modal from "../components/Modal"
 import Nav from "../components/Navigation"
 import World from "../media/images/world.jpg"
+
+import styles from "./styles/modalstyles.module.css"
 
 const Image = styled.img`
   width: 200px;
   height: 200px;
 `
 
-export default () => (
-  <Layout>
-    <Nav text="World Kind" />
-    <Modal />
-    <Image src={World} alt="" />
-  </Layout>
-)
+// either useState or use props to open / close the modal
+export default function Homepage() {
+  // ReactModal.setAppElement(el)
+  return (
+    <Layout>
+      <Nav text="World Kind" />
+      <ReactModal
+        isOpen={true}
+        onRequestClose={() => {}}
+        contentLabel="Information Modal about <insert name here>"
+        // style={{
+        //   overlay: { backgroundColor: "pink" }, // what happens to the page when modal is open
+        //   content: { backgroundColor: "red", opacity: "0.5" },
+        // }}
+        className={styles.content_body}
+      />
+
+      <Image src={World} alt="" />
+    </Layout>
+  )
+}
