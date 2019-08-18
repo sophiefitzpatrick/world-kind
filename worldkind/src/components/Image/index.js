44dedab9f7657fda5404feef896715fc11af64d1
rachelfranklin1 <rachel.franklin@marvelapp.com>
2019-08-18 13:27:02 +0100

    moving components to their own folders

---
+++ worldkind/src/components/Image/index.js
@@ -0,0 +1,6 @@
+import styled from "styled-components"
+
+const Image = styled.img`
+  border-radius: 20%;
+`
+export default Image
--- worldkind/src/pages/index.js
+++ worldkind/src/pages/index.js
@@ -1,126 +1,26 @@
-import React, { useState } from "react"
+import React from "react"
 import styled from "styled-components"
-import ReactModal from "react-modal"
 
-import CloseIcon from "../components/Icons/CloseIcon"
+import Image from "../components/Image"
 import Layout from "../components/Layout"
-import Nav from "../components/Navigation"
-import World from "../media/images/world.jpg"
+import Heading from "../components/Heading"
+import LandingEarth from "../media/gifs/landing-earth-sparkles.gif"
 
-import styles from "./styles/modalstyles.module.css"
-
-const Image = styled.img`
-  width: 200px;
-  height: 200px;
-  border-radius: 20%;
-`
-const FeaturedImageCol = styled.div`
-  width: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: ${props =>
-    props.backgroundColor ? props.backgroundColor : `white`};
-
-  @media (max-width: 760px) {
-    height: 50%;
-    width: 100%;
-    padding: 10%;
-  }
-`
-const FeatureInformationCol = styled.div`
-  width: 50%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  background-color: white;
-
-  @media (max-width: 760px) {
-    height: 50%;
-    width: 100%;
-  }
-`
-
-const ColumnWrap = styled.div`
-  padding: 10%;
-  width: 100%;
-  height: 100%;
-`
-const Paragraph = styled.p`
-  font-size: 1.25rem;
-  color: #0a132b;
-  font-family: Montserrat;
-`
-const ColumnHeading = styled.h1`
-  font-size: 3rem;
-  color: #0a132b;
-  font-family: Montserrat;
-`
-
-const CloseButton = styled.button`
-  width: 100%;
-  border: none;
-  background-color: white;
-  margin-top: -1rem;
-  margin-right: -1rem;
-  display: flex;
-  justify-content: flex-end;
-  outline: none;
-  cursor: pointer;
+const MaxWidth = styled.div`
+  max-width: 50%;
 `
 
 export default function Homepage() {
-  const [isModalOpen, setIsModalOpen] = useState(false)
+  // const [isModalOpen, setIsModalOpen] = useState(false)
   return (
     <Layout>
-      <Nav text="World Kind" />
-      <Image
-        src={World}
-        alt=""
-        onClick={() => {
-          setIsModalOpen(true)
-        }}
-      />
-      <ReactModal
-        isOpen={isModalOpen}
-        onRequestClose={() => {
-          setIsModalOpen(false)
-        }}
-        // ^^ this will also handle the transition
-        contentLabel="Information Modal about <insert name here>"
-        overlayClassName={styles.content_overlay}
-        className={styles.content_body}
-      >
-        <FeaturedImageCol backgroundColor="#4FB0C6">
-          <Image src={World} alt="illustration of humans on top of earth" />
-        </FeaturedImageCol>
-        <FeatureInformationCol>
-          <ColumnWrap>
-            <CloseButton
-              onClick={() => {
-                setIsModalOpen(false)
-              }}
-            >
-              <CloseIcon />
-            </CloseButton>
-
-            <ColumnHeading>Name of Charity</ColumnHeading>
-            <Paragraph>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-              harum nobis doloribus culpa tempore ratione atque accusantium
-              magni delectus, tempora dolorem, exercitationem deserunt quasi
-              quidem et vero. Id, aliquam enim!
-            </Paragraph>
-            <Paragraph>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-              harum nobis doloribus culpa tempore ratione atque accusantium
-              magni delectus, tempora dolorem, exercitationem deserunt quasi
-              quidem et vero. Id, aliquam enim! Lorem ipsum, dolor sit amet
-              consectetur adipisicing elit. Ipsa, velit iusto.
-            </Paragraph>
-          </ColumnWrap>
-        </FeatureInformationCol>
-      </ReactModal>
+      <MaxWidth>
+        <Heading size="4rem">WorldKind</Heading>
+        <Heading size="2rem">Empowering big impact with small actions</Heading>
+      </MaxWidth>
+      <a href="/about">
+        <Image src={LandingEarth} alt="" />
+      </a>
     </Layout>
   )
 }
