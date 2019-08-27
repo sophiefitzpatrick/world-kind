3316c5944f95a5ff6651c32c8d53e5f52bef4ff9
rachelfranklin1 <rachel.franklin@marvelapp.com>
2019-08-27 21:43:50 +0100

    alignment

--- worldkind/src/pages/about.js
+++ worldkind/src/pages/about.js
@@ -12,9 +12,10 @@ import Turtle from "../media/images/turtle.jpg"
 const Hero = styled.div`
   width: 100%;
   height: 600px;
-  padding: 80px 0 0 0;
+  padding: 80px 0 0 10%;
   display: grid;
-  grid-template-columns: 60% 40%;
+  grid-template-columns: 55% 45%;
+  background-color: #131515;
 `
 
 export default function Homepage() {
@@ -28,11 +29,9 @@ export default function Homepage() {
             height: "100%",
             width: "100%",
             color: "white",
-            backgroundColor: "#131515",
             display: "flex",
             flexDirection: "column",
             justifyContent: "center",
-            alignItems: "center",
           }}
         >
           <MaxWidth width="60">
@@ -58,8 +57,9 @@ export default function Homepage() {
           width: "100%",
           margin: 0,
           display: "flex",
-          justifyContent: "center",
+          justifyContent: "start",
           alignItems: "center",
+          paddingLeft: "10%",
         }}
       >
         <MaxWidth width="80">
